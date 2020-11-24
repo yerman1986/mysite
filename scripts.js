406eb2508b14458bdb1ed3f26f57e7a28d2d2ca5
yerman1986 <36877592+yerman1986@users.noreply.github.com>
2020-11-24 19:58:52 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function meth() {
+
+	var login=document.getElementById("login").value;
+	alert(login + " you are\nregistered successfully!");
+
+	
+	var header=document.querySelector('h1');
+	header.textContent = "Congritulations ourself!" 
+	+"\nYou are registered now!";
+	
+	document.getElementById ("text").textContent="";
+	
+	
+	
+
+}
+ 
